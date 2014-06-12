bce8caf8bd4458db4a3859a7288a978567597273
rupertbates <rupert.bates@guardian.co.uk>
2014-06-12 18:29:58 +0100

    Part 2 of assignment 1

--- complete.R
+++ complete.R
@@ -12,4 +12,14 @@ complete <- function(directory, id = 1:332) {
   ## ...
   ## where 'id' is the monitor ID number and 'nobs' is the
   ## number of complete cases
+  getComplete <- function(id){
+    filename <- paste(directory, "/", formatC(id, width=3, flag="0"), ".csv", sep="")  
+    #print(filename)
+    data <- read.csv(filename)
+    good <- complete.cases(data)
+    nrow(data[good,])
+  }
+  
+  complete <- sapply(id, getComplete)
+  data.frame(id=id, nobs=complete)
 }
