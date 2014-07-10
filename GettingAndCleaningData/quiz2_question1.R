cee66ed11e73e2ac0421b1fb7dc4f00faef5e493
rbates <rupert.bates@gmail.com>
2014-07-10 21:25:16 +0100

    Start week 2 quiz

---
+++ GettingAndCleaningData/quiz2_question1.R
@@ -0,0 +1,28 @@
+library(httr)
+library(httpuv)
+
+# 1. Find OAuth settings for github:
+#    http://developer.github.com/v3/oauth/
+oauth_endpoints("github")
+
+# 2. Register an application at https://github.com/settings/applications
+#    Insert your values below - if secret is omitted, it will look it up in
+#    the GITHUB_CONSUMER_SECRET environmental variable.
+#
+#    Use http://localhost:1410 as the callback url
+myapp <- oauth_app("github", "6f2545d557c8984a3fff", secret="5c89b09e49273984327561f4fab927ab795eb1c5")
+
+# 3. Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# 4. Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+stop_for_status(req)
+cont <- content(req)
+sapply(cont, function(el){el$name})
+
+# OR:
+req <- with_config(gtoken, GET("https://api.github.com/rate_limit"))
+stop_for_status(req)
+content(req)
