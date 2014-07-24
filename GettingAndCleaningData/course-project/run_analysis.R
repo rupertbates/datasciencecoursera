74fb553215c445124902f4c516cd3b5cad08d5e8
rupertbates <rupert.bates@guardian.co.uk>
2014-07-24 16:35:26 +0100

    Course project

---
+++ GettingAndCleaningData/course-project/run_analysis.R
@@ -0,0 +1,55 @@
+# You should create one R script called run_analysis.R that does the following. 
+# Merges the training and the test sets to create one data set.
+# Extracts only the measurements on the mean and standard deviation for each measurement. 
+# Uses descriptive activity names to name the activities in the data set
+# Appropriately labels the data set with descriptive variable names. 
+# Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+
+
+#Load in the lists of features and activities
+features <- read.table("./course-project/UCI HAR Dataset/features.txt", stringsAsFactors=FALSE)
+featureNames <- features[,2]
+activityNames <- read.table("./course-project/UCI HAR Dataset/activity_labels.txt", col.names=c("activityId", "activity"))
+
+# Get a full list of names for the measurement columns and also a list of columns which contain mean and standard deviation measurements
+regex <- "^.*mean().*|^.*std().*"
+meanStdCols <- grep(regex, featureNames, value=TRUE)
+
+# The list of columns in the tidied dataframe
+colNames <- c(c("subject", "activity"), meanStdCols)
+
+
+loadAndTidyDataset <- function(x){
+  # Load the full data set
+  data <- read.table(paste0("./course-project/UCI HAR Dataset/", x, "/X_", x, ".txt"), col.names=featureNames)
+  
+  # Reduce the data frame to just those columns containing mean and standard deviation measurements
+  data <- data[grep(regex, names(data), value=TRUE)]
+  
+  # Add the subject and activity into the dataset
+  subject <- read.table(paste0("./course-project//UCI HAR Dataset/", x, "/subject_", x, ".txt"), col.names=c("subject"))
+  activity <- read.table(paste0("./course-project/UCI HAR Dataset/", x, "/y_", x, ".txt"), col.names=c("activityId"))
+  activityMerged <- merge(activityNames, activity, all.y=TRUE)
+  dataWithSubject <- data.frame(subject, activityMerged$activity, data)
+  
+  # Add descriptive names to the columns
+  names(dataWithSubject) <- colNames
+  dataWithSubject 
+}
+
+
+test <- loadAndTidyDataset("test")
+train <- loadAndTidyDataset("train")
+
+
+
+# Merge the datasets
+merged <- rbind(test, train)
+
+# Extract only mean and standard deviation measurements
+regex <- "^.*mean().*|^.*std().*"
+reduced <- merged[grep(regex, names(merged), value=TRUE)]
+
+
+
+
