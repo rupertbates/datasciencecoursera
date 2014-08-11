ce8efd7abdde3a4c21138c8a2b97a32139843b48
rbates <rupert.bates@gmail.com>
2014-08-11 17:34:23 +0200

    Split the first 2 plots of project 2 into individual R files

---
+++ ExploratoryDataAnalysis/plot1.R
@@ -0,0 +1,11 @@
+# Question 1
+
+NEI <- readRDS("summarySCC_PM25.rds")
+NEI$YearDate <- strptime(NEI$year, format="%Y")
+
+emissionsByYear <- aggregate(Emissions ~ year, data=NEI, sum)
+
+png(filename="plot1.png", width=500, height=500)
+plot(emissionsByYear, type="o", main="Total PM2.5 emissions in the United States", xaxt="n")
+axis(side=1, at=c(1999,2002,2005,2008), tick=FALSE,  labels=c("1999", "2002", "2005", "2008"))
+dev.off()
---
+++ ExploratoryDataAnalysis/plot2.R
@@ -0,0 +1,12 @@
+# Question 2
+
+NEI <- readRDS("summarySCC_PM25.rds")
+NEI$YearDate <- strptime(NEI$year, format="%Y")
+
+baltimore <- NEI[NEI$fips=="24510", ]
+baltimoreEmissionsByYear <- aggregate(Emissions ~ year, data=baltimore, sum)
+
+png(filename="plot2.png", width=500, height=500)
+plot(baltimoreEmissionsByYear, type="o", main="Total PM2.5 emissions - Baltimore", xaxt="n")
+axis(side=1, at=c(1999,2002,2005,2008), tick=FALSE,  labels=c("1999", "2002", "2005", "2008"))
+dev.off()
